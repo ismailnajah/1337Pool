59dbe1630c009300350191fbe5bbe1a04e0876d5
Ismail Najah <inajah@e1r6p2.1337.ma>
2024-06-25 12:11:07 +0100

    undo adding variables to .sh

--- Shell01/ex01/print_groups.sh
+++ Shell01/ex01/print_groups.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-FT_USER=daemon
 id -Gn $FT_USER | tr ' ' ',' | tr -d '\n'
--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-FT_LINE1=4
-FT_LINE2=7
 cat /etc/passwd | awk '!/^#.*$/' | sed -n 'n;p' | sed 's/:.*//g' | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/\./' | tr -d '\n'
