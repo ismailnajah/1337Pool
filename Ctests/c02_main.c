b5d08dc3b99191c7722c160b466d68f4ea7821f4
Ismail Najah <inajah@e1r5p2.1337.ma>
2024-06-27 18:20:11 +0100

    [Solved] C02: ex03

--- Ctests/c02_main.c
+++ Ctests/c02_main.c
@@ -4,6 +4,8 @@
 
 #include "../C02/ex00/ft_strcpy.c"
 #include "../C02/ex01/ft_strncpy.c"
+#include "../C02/ex02/ft_str_is_alpha.c"
+#include "../C02/ex03/ft_str_is_numeric.c"
 
 int main()
 {
@@ -20,6 +22,25 @@ int main()
 	char dest12[16];
 	printf("original strncpy : %s\n", strncpy(dest11, src, 5));
 	printf("  our ft_strncpy : %s\n", ft_strncpy(dest12, src, 5));
+	printf("\n");
+
+	//testing ex02
+	char *nodigits = "this string has no digits.";
+	char *withdigits = "this string has 4 digits. 1337";
+	char *empty = "";
+	char *onlydigits = "4324345553222";
+	printf("ft_str_is_alpha(\"%s\"): %d\n", nodigits, ft_str_is_alpha(nodigits));
+	printf("ft_str_is_alpha(\"%s\"): %d\n", withdigits, ft_str_is_alpha(withdigits));
+	printf("ft_str_is_alpha(\"%s\"): %d\n", empty, ft_str_is_alpha(empty));
+	printf("ft_str_is_alpha(\"%s\"): %d\n", onlydigits, ft_str_is_alpha(onlydigits));
+	printf("\n");
+
+	//testing ex03
+	printf("ft_str_is_numeric(\"%s\"): %d\n", nodigits, ft_str_is_numeric(nodigits));
+	printf("ft_str_is_numeric(\"%s\"): %d\n", withdigits, ft_str_is_numeric(withdigits));
+	printf("ft_str_is_numeric(\"%s\"): %d\n", empty, ft_str_is_numeric(empty));
+	printf("ft_str_is_numeric(\"%s\"): %d\n", onlydigits, ft_str_is_numeric(onlydigits));
+	
 
 
 	return 0;
