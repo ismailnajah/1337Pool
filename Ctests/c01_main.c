0f80ba22dead06a833f65b22566261eaf68c4f1b
Ismail Najah <inajah@e1r5p2.1337.ma>
2024-06-27 11:36:06 +0100

    [Solved] C01: ex03

--- Ctests/c01_main.c
+++ Ctests/c01_main.c
@@ -3,8 +3,8 @@
 
 #include "../C01/ex00/ft_ft.c"
 #include "../C01/ex01/ft_ultimate_ft.c"
-//#include "../C01/ex02/ft_swap.c"
-//#include "../C01/ex03/ft_div_mod.c"
+#include "../C01/ex02/ft_swap.c"
+#include "../C01/ex03/ft_div_mod.c"
 //#include "../C01/ex04/ft_ultimate_div_mod.c"
 //#include "../C01/ex05/ft_putstr.c"
 //#include "../C01/ex06/ft_strlen.c"
@@ -14,12 +14,42 @@
 
 int main()
 {
-	int a;
+	//testing ex00
+	int a = 1337;
+	printf("a = %d\n", a);
 	ft_ft(&a);
-	printf("ft_ft : %d\n", a);
+	printf("ft_ft : %d\n\n", a);
+
+	//testing ex01
+	a = 59543;
+	printf("a = %d\n", a);
+	int *a1 = &a;
+	int **a2 = &a1;
+	int ***a3 = &a2;
+	int ****a4 = &a3;
+	int *****a5 = &a4;
+	int ******a6 = &a5;
+	int *******a7 = &a6;
+	int ********a8 = &a7;
+	int *********a9 = &a8;
+	ft_ultimate_ft(a9);
+	printf("ft_ultimate_ft: %d\n\n", a);	
+
+	//testing ex02
+	int b = 1337;
+	int c = 4242;
+	printf("b=%d, c=%d\n",b,c);
+	ft_swap(&b, &c);
+	printf("ft_swap: b=%d, c=%d", b, c);
 	
-	int ultimate;
-	ft_ultimate_ft( &&&&&&&&&ultimate );
-	printf("ft_ultimate_ft: %d\n", ultimate);d	
+	//testing ex03
+	int diva =  513;
+	int divb = 50;
+	int div;
+	int mod;
+	ft_div_mod(diva, divb, &div, &mod);
+	printf("ft_div_mod: %d / %d  = %d | mod = %d", diva, divb, div, mod);
+	
+	
 	return 0;
 }
