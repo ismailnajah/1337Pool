53501a7b0ba59612965f084a4648550a23a4027e
Ismail Najah <inajah@e1r5p2.1337.ma>
2024-06-27 13:29:34 +0100

    [Solved] C01: ex08

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inajah <inajah@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 13:09:00 by inajah            #+#    #+#             */
+/*   Updated: 2024/06/27 13:28:41 by inajah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	swaps;
+	int	temp;
+
+	swaps = 1;
+	while (swaps > 0)
+	{
+		swaps = 0;
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+				swaps++;
+			}
+			i++;
+		}
+	}
+}
