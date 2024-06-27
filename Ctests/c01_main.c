6da579cb4e4835377b57af430d7c8ae0a49246f7
Ismail Najah <inajah@e1r5p2.1337.ma>
2024-06-27 10:44:21 +0100

    [info] setting up enviroment for C01

---
+++ Ctests/c01_main.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+
+#include "../C01/ex00/ft_ft.c"
+#include "../C01/ex01/ft_ultimate_ft.c"
+//#include "../C01/ex02/ft_swap.c"
+//#include "../C01/ex03/ft_div_mod.c"
+//#include "../C01/ex04/ft_ultimate_div_mod.c"
+//#include "../C01/ex05/ft_putstr.c"
+//#include "../C01/ex06/ft_strlen.c"
+//#include "../C01/ex07/ft_rev_int_tab.c"
+//#include "../C01/ex08/ft_sort_int_tab.c"
+
+
+int main()
+{
+	int a;
+	ft_ft(&a);
+	printf("ft_ft : %d\n", a);
+	
+	int ultimate;
+	ft_ultimate_ft( &&&&&&&&&ultimate );
+	printf("ft_ultimate_ft: %d\n", ultimate);d	
+	return 0;
+}
