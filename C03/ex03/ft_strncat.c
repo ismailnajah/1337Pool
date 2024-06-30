288b12eca756af958a547328259b3472e0bca33a
Ismail Najah <inajah@e1r8p6.1337.ma>
2024-06-30 22:17:35 +0100

    [Solved] C03: ex03

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inajah <inajah@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/30 21:42:17 by inajah            #+#    #+#             */
+/*   Updated: 2024/06/30 22:14:20 by inajah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	size_dest;
+	unsigned int	i;
+
+	size_dest = 0;
+	while (dest[size_dest])
+		size_dest++;
+	i = 0;
+	while (i < nb && src[i])
+	{
+		dest[size_dest + i] = src[i];
+		i++;
+	}
+	dest[size_dest + i] = '\0';
+	return (dest);
+}
--- Ctests/c03_main.c
+++ Ctests/c03_main.c
@@ -4,6 +4,7 @@
 #include "../C03/ex00/ft_strcmp.c"
 #include "../C03/ex01/ft_strncmp.c"
 #include "../C03/ex02/ft_strcat.c"
+#include "../C03/ex03/ft_strncat.c"
 
 int main()
 {
@@ -21,12 +22,19 @@ int main()
 	printf("\n");
 
 
-	char z1[10] = "hello ";
+	char s11[20] = "hello ";
 	printf("\tstrcat(\"%s\", \"%s\") : %s\n", s1, s2, strcat(s1, s2));
-	printf("\tft_strcmp(\"%s\", \"%s\") : %s\n", z1, s2, ft_strcat(z1, s2));
+	printf("\tft_strcmp(\"%s\", \"%s\") : %s\n", s11, s2, ft_strcat(s11, s2));
 	printf("\n");
 	
-
+	char cat_s1[20] = "hello ";
+	char cat_z1[20] = "hello ";
+	char cat_src[]= {'h','e','l', 'l', 'b', 'c', '0'};
+	unsigned int nb = 50;
+	printf("\tstrcat(\"%s\", \"%s\") : %s\n", cat_s1, cat_src, strncat(cat_s1, cat_src, nb));
+	printf("\tft_strcmp(\"%s\", \"%s\") : %s\n", cat_z1, cat_src, ft_strncat(cat_z1, cat_src, nb));
+	printf("\n");
+	
 #endif
 	return 0;
 }
