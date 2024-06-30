fa264b30ea78cb686ec5be5c485a74b9000399a0
Ismail Najah <inajah@e1r8p6.1337.ma>
2024-06-30 21:41:32 +0100

    [Solved] C03: ex02

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inajah <inajah@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/30 21:14:17 by inajah            #+#    #+#             */
+/*   Updated: 2024/06/30 21:41:01 by inajah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcat(char *dest, char *src)
+{
+	unsigned int	i;
+	unsigned int	size_dest;
+
+	size_dest = 0;
+	while (dest[size_dest])
+		size_dest++;
+	i = 0;
+	while (src[i])
+	{
+		dest[size_dest + i] = src[i];
+		i++;
+	}
+	dest[size_dest + i] = '\0';
+	return (dest);
+}
--- Ctests/c03_main.c
+++ Ctests/c03_main.c
@@ -3,20 +3,29 @@
 
 #include "../C03/ex00/ft_strcmp.c"
 #include "../C03/ex01/ft_strncmp.c"
+#include "../C03/ex02/ft_strcat.c"
+
 int main()
 {
 #if 1
-	char s1[] = "helle";
-	char s2[] = "hell";
+	char s1[20] = "hello ";
+	char s2[]= {'h','e','l', 'l', 'b', 'c', 0};
+
 	printf("\tstrcmp(\"%s\", \"%s\") : %d\n", s1, s2, strcmp(s1, s2));
 	printf("\tft_strcmp(\"%s\", \"%s\") : %d\n", s1, s2, ft_strcmp(s1, s2));
 	printf("\n");
 	
-	unsigned int n = 5;
+	unsigned int n = 4;
 	printf("\tstrncmp(\"%s\", \"%s\") : %d\n", s1, s2, strncmp(s1, s2, n));
 	printf("\tft_strncmp(\"%s\", \"%s\") : %d\n", s1, s2, ft_strncmp(s1, s2, n));
+	printf("\n");
 
 
+	char z1[10] = "hello ";
+	printf("\tstrcat(\"%s\", \"%s\") : %s\n", s1, s2, strcat(s1, s2));
+	printf("\tft_strcmp(\"%s\", \"%s\") : %s\n", z1, s2, ft_strcat(z1, s2));
+	printf("\n");
+	
 
 #endif
 	return 0;
