5f449b71a49fbd8c2206cc8021a320e8a38d26ee
Ismail Najah <inajah@e1r8p6.1337.ma>
2024-07-01 12:01:44 +0100

    [Solved] C03: ex05

---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inajah <inajah@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 08:38:19 by inajah            #+#    #+#             */
+/*   Updated: 2024/07/01 12:00:52 by inajah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_copy(char *dest, char *src, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+
+	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dest);
+	if (size <= dest_len)
+		return (size + src_len);
+	i = 0;
+	if (src_len < size - dest_len)
+	{
+		ft_copy(dest + dest_len, src, src_len);
+		dest[dest_len + src_len] = '\0';
+	}
+	else
+	{
+		ft_copy(dest + dest_len, src, size - 1);
+		dest[dest_len + size] = '\0';
+	}
+	return (dest_len + src_len);
+}
--- Ctests/c03_main.c
+++ Ctests/c03_main.c
@@ -6,10 +6,13 @@
 #include "../C03/ex02/ft_strcat.c"
 #include "../C03/ex03/ft_strncat.c"
 #include "../C03/ex04/ft_strstr.c"
+#include "../C03/ex05/ft_strlcat.c"
+
+#include "../C00/ex07/ft_putnbr.c"
 
 int main()
 {
-#if 1
+#if 0
 	char s1[20] = "hello ";
 	char s2[]= {'h','e','l', 'l', 'b', 'c', 0};
 
@@ -25,22 +28,37 @@ int main()
 
 	char s11[20] = "hello ";
 	printf("\tstrcat(\"%s\", \"%s\") : %s\n", s1, s2, strcat(s1, s2));
-	printf("\tft_strcmp(\"%s\", \"%s\") : %s\n", s11, s2, ft_strcat(s11, s2));
+	printf("\tft_strcat(\"%s\", \"%s\") : %s\n", s11, s2, ft_strcat(s11, s2));
 	printf("\n");
 	
 	char cat_s1[20] = "hello ";
 	char cat_z1[20] = "hello ";
 	char cat_src[]= {'h','e','l', 'l', 'b', 'c', '0'};
 	unsigned int nb = 50;
-	printf("\tstrcat(\"%s\", \"%s\") : %s\n", cat_s1, cat_src, strncat(cat_s1, cat_src, nb));
-	printf("\tft_strcmp(\"%s\", \"%s\") : %s\n", cat_z1, cat_src, ft_strncat(cat_z1, cat_src, nb));
+	printf("\tstrncat(\"%s\", \"%s\") : %s\n", cat_s1, cat_src, strncat(cat_s1, cat_src, nb));
+	printf("\tft_strncat(\"%s\", \"%s\") : %s\n", cat_z1, cat_src, ft_strncat(cat_z1, cat_src, nb));
 	printf("\n");
 
 	char haystack[] = "this is heystack string";
 	char needle[] = "tack";
 	printf("\tstrstr(\"%s\", \"%s\") : %s\n", haystack, needle, strstr(haystack, needle));
 	printf("\tft_strstr(\"%s\", \"%s\") : %s\n", haystack, needle, ft_strstr(haystack, needle));
+	printf("\n");
+
+#endif
+#if 1
+	char lcat_s1[50] = "hello world";
+	char lcat_z1[50] = "hello world";
+	char lcat_src[]= "best";
+	int dest_size = 3; 
+	printf("\tstrlcat(\"%s\", \"%s\", %d) : ",lcat_s1, lcat_src, dest_size);
+	int result = strlcat(lcat_s1, lcat_src, dest_size); 
+	printf("%s ->  %d\n", lcat_s1, result);
 	
+	printf("\tft_strlcat(\"%s\", \"%s\", %d) : ",lcat_z1, lcat_src, dest_size);
+	result = ft_strlcat(lcat_z1, lcat_src, dest_size); 
+	printf("%s ->  %d\n", lcat_z1, result);
+	printf("\n");
 	
 #endif
 	return 0;
