ac00a76eac609cd6b62fad00ab30a979efe7c51a
Ismail Najah <inajah@e1r4p5.1337.ma>
2024-07-02 09:12:03 +0100

    [Solved] C04: ex00, ex01 and ex02

---
+++ Ctests/c04_main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+#include "../C04/ex00/ft_strlen.c"
+#include "../C04/ex01/ft_putstr.c"
+#include "../C04/ex02/ft_putnbr.c"
+
+#define NL write(1, "\n", 1)
+
+int main()
+{
+	char *str = "testing ft_strlen\n";
+	printf("ft_strlen = %d, strlen = %ld\n", ft_strlen(str), strlen(str));
+	ft_putstr(str);
+	ft_putnbr(INT_MIN);
+	NL;
+	ft_putnbr(INT_MAX);
+	NL;
+	ft_putnbr(-1337);
+	NL;
+	ft_putnbr(4242);
+	NL;
+	ft_putnbr(0);
+
+	return 0;
+}
