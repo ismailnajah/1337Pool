da8818eab9f5b828f767b01da405de94f0a880c8
Ismail Najah <inajah@e1r1p9.1337.ma>
2024-07-07 08:44:26 +0100

    [Solved] C08: ex01

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -1,21 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: inajah <inajah@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/06 20:09:56 by inajah            #+#    #+#             */
-/*   Updated: 2024/07/06 21:07:09 by inajah           ###   ########.fr       */
+/*   Created: 2024/07/06 20:51:53 by inajah            #+#    #+#             */
+/*   Updated: 2024/07/07 08:43:50 by inajah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
 
-#ifndef FT_H
-# define FT_H
+typedef enum e_bool
+{
+	FALSE = 0,
+	TRUE	
+}	t_bool;
 
-void	ft_putchar(char c);
-void	ft_swap(int *a, int *b);
-void	ft_putstr(char *str);
-int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
+# define EVEN(nbr)  (nbr) % 2 == 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
 #endif
