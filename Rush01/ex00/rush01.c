8939f0340e1fa12e463fceed7b2ebb59f2a911be
Ismail Najah <inajah@e2r1p9.1337.ma>
2024-07-07 16:03:59 +0100

    [Solved] Rush01: not following norminette yet

--- Rush01/ex00/rush01.c
+++ Rush01/ex00/rush01.c
@@ -6,7 +6,7 @@
 /*   By: inajah <inajah@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 09:50:28 by inajah            #+#    #+#             */
-/*   Updated: 2024/07/07 14:28:14 by inajah           ###   ########.fr       */
+/*   Updated: 2024/07/07 16:00:34 by inajah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,30 @@ int	ft_print_solution()
 		while (c < g_size - 1)
 		{
 			write(1, &"0123456789"[g_grid[r][c]], 1);
-			if(c == g_size - 2)
+			if(c != g_size - 2)
 				write(1," ",1);
 			c++;
 		}
 		r++;
 		write(1,"\n",1);
-	}
+	}	
 	return (1);
 }
 
-int	ft_isvalid_row(int c, int dir, int rule_index)
+int	ft_isvalid_row(int col, int dir, int rule_index)
 {
 	int count_maxs;
 	int max;
 	int r;
+	int c;
 
 	r = 1;
-	count_maxs = 0;	
-	max = 0;
+	c = col;
 	while (r < g_size - 1)
 	{
+		count_maxs = 0;
+		max = 0;
+		c = col;
 		while(0 < c && c < g_size - 1)
 		{
 			if (max < g_grid[r][c])
@@ -67,17 +70,20 @@ int	ft_isvalid_row(int c, int dir, int rule_index)
 	return (1);
 }
 
-int	ft_isvalid_col(int r, int dir, int rule_index)
+int	ft_isvalid_col(int row, int dir, int rule_index)
 {
 	int count_maxs;
 	int max;
 	int c;
+	int r;
 
 	c = 1;
-	count_maxs = 0;	
-	max = 0;
+	r = row;
 	while (0 < c && c < g_size - 1)
 	{
+		count_maxs = 0;
+		max = 0;
+		r = row;
 		while(0 < r && r < g_size - 1)
 		{
 			if (max < g_grid[r][c])
@@ -102,10 +108,10 @@ int ft_validate_solution()
 	int col_fw;
 	int col_bw;
 
-	row_fw = ft_isvalid_row(1, FW, 0);
-	row_bw = ft_isvalid_row(g_size - 2, BW, g_size - 1);
-	col_fw = ft_isvalid_col(1, FW, 0);
-	col_bw = ft_isvalid_col(g_size - 2, BW, g_size - 1);
+	row_fw = ft_isvalid_row(1, 1, 0);
+	row_bw = ft_isvalid_row(g_size - 2, -1, g_size - 1);
+	col_fw = ft_isvalid_col(1, 1, 0);
+	col_bw = ft_isvalid_col(g_size - 2, -1, g_size - 1);
 	return row_fw && row_bw && col_fw && col_bw;
 }
 
@@ -139,25 +145,28 @@ int	ft_solution(int r, int c)
 	int next_cell_r;
 	int next_cell_c;
 
-	if (r == g_size - 2 && c == g_size - 1)
+	if (r == g_size - 1)
 	{
 		if (ft_validate_solution())
 			return (ft_print_solution());
 		else
-			return (0);	
+			return (0);
 	}
-	g_grid[r][c] += 1;	
+	g_grid[r][c] += 1;
 	while(!ft_suduko(r, c) && g_grid[r][c] < g_size - 1)
 		g_grid[r][c]++;
 	if(g_grid[r][c] == g_size - 1)
-		return 0;	
+	{
+		g_grid[r][c] = 0;
+		return 0;
+	}
 	next_cell_c = c + 1;
 	next_cell_r = r;
 	if(next_cell_c == g_size - 1){
 		next_cell_c = 1;
 		next_cell_r = r + 1;
 	}
-	if (!ft_solution(next_cell_r, next_cell_c))
-		return ft_solution(r, c);
+	if(!ft_solution(next_cell_r, next_cell_c))
+		return ft_solution(r,c);	
 	return (1);
 }
