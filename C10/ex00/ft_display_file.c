cb3d0beebf0e471e3f5edd26dced2d1d14a423ef
Ismail Najah <inajah@e2r2p8.1337.ma>
2024-07-09 17:47:43 +0100

    [Info] C10: ex00 initial solution

--- C10/ex00/ft_display_file.c
+++ C10/ex00/ft_display_file.c
@@ -6,7 +6,7 @@
 /*   By: inajah <inajah@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 17:00:46 by inajah            #+#    #+#             */
-/*   Updated: 2024/07/09 17:34:11 by inajah           ###   ########.fr       */
+/*   Updated: 2024/07/09 17:46:01 by inajah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,12 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_putstr(char *str)
+void	ft_putnstr(char *str, int size)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (i < size)
 	{
 		ft_putchar(str[i]);
 		i++;
@@ -53,9 +53,13 @@ int	main(int ac, char **av)
 	
 	int	fd = open(av[1], O_RDONLY);
 	if (fd < 0)
-		return (ft_error("Cannot read file."));
-	read(fd, buffer, sizeof(buffer));
-	ft_putstr(buffer);	
+		return (ft_error("Cannot read file.\n"));
+	int len = 1;
+	while (len > 0)
+	{
+		len = read(fd, buffer, sizeof(buffer));
+		ft_putnstr(buffer, len);
+	}
 	close(fd);
 	return (0);
 }
