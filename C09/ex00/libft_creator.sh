478892c00467bbd526a5f00fed94d5d4928870ca
Ismail Najah <inajah@e1r4p6.1337.ma>
2024-07-09 11:43:24 +0100

    [Fixed] C09: ex00 moulinette didn't like my shell script

--- C09/ex00/libft_creator.sh
+++ C09/ex00/libft_creator.sh
@@ -6,21 +6,12 @@
 #    By: inajah <inajah@student.1337.ma>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/07/08 13:15:43 by inajah            #+#    #+#              #
-#    Updated: 2024/07/08 14:50:02 by inajah           ###   ########.fr        #
+#    Updated: 2024/07/09 11:41:34 by inajah           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
-C_SOURCE=(ft_putchar ft_swap ft_putstr ft_strlen ft_strcmp)
-O_FILES=''
-CFLAGS='-Wall -Wextra -Werror'
-
-for name in ${C_SOURCE[@]};
-do
-	echo "gcc $CFLAGS -c $name.c -o $name.o"
-	gcc $CFLAGS -c $name.c -o $name.o
-	O_FILES+="$name.o "
-done
-echo "libtool $O_FILES -o libft.a"
-libtool $O_FILES -o libft.a
-rm $O_FILES
+rm -f libft.a
+find . -name "*.c" -type f -exec gcc -Wall -Werror -Wextra -c {} \;
+ar rcs libft.a *.o
+find . -name "*.o" -type f -delete
