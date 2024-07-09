0b78839886ddc4cfd59184a900a332a3d2df3933
Ismail Najah <inajah@e1r4p6.1337.ma>
2024-07-09 09:54:41 +0100

    [Solved] C09: ex02

---
+++ C09/ex02/ft_split.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inajah <inajah@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 09:07:22 by inajah            #+#    #+#             */
+/*   Updated: 2024/07/09 09:54:02 by inajah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#define MAX_ASCII 255
+
+void	ft_init_split(int *hash_t, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (i < MAX_ASCII)
+		hash_t[i++] = 0;
+	i = 0;
+	while(charset[i])
+	{
+		hash_t[(unsigned char) charset[i]] = 1;
+		i++;
+	}
+}
+
+char	*ft_strndup(char *str, int size)
+{
+	char	*copy;
+	int		i;
+
+	copy = malloc((size + 1) * sizeof(char));
+	if (!copy)
+		return NULL;
+	i = 0;
+	while (i < size)
+	{
+		copy[i] = str[i];
+		i++;
+	}
+	copy[size] = '\0';
+	return copy;
+}
+
+char	**ft_count_words(char *str, int *hash_t, int *words_count)
+{
+	int		i;
+	char	**words_list;
+
+	i = 0;
+	while(str[i])
+	{
+		if (hash_t[(unsigned char) str[i]] == 1)
+			i++;
+		else
+		{
+			*words_count = *words_count + 1;
+			while (str[i] && hash_t[(unsigned char) str[i]] == 0)
+				i++;
+		}
+	}
+	words_list = malloc((*words_count + 1) * sizeof(char *));
+	if (!words_list)
+		return NULL;
+	return words_list;
+}
+
+void	_ft_split(char *str, int *hash_t, char **words_list, int words_count)
+{
+	int start;
+	int i;
+	int	index;
+
+	i = 0;
+	index = 0;
+	while (index < words_count)
+	{
+		if(hash_t[(unsigned char) str[i]] == 1)
+			i++;
+		else
+		{
+			start = i;
+			while (str[i] && hash_t[(unsigned char) str[i]] == 0)
+				i++;
+			if (i > start)
+			{
+				words_list[index] = ft_strndup(str+start, i - start);
+				index++;
+			}
+		}
+	}
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	int		hash_t[MAX_ASCII];
+	int		words_count;
+	char	**words_list;
+
+	ft_init_split(hash_t, charset);
+	words_count = 0;
+	words_list = ft_count_words(str, hash_t, &words_count);
+	if (!words_list)
+		return (NULL);
+	_ft_split(str, hash_t, words_list, words_count);
+	words_list[words_count] = 0;
+	return (words_list);
+}
--- Ctests/c09_main.c
+++ Ctests/c09_main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include "../C09/ex02/ft_split.c"
 
+#define ON 0
+
+#if ON
 void ft_putchar(char c);
 void ft_swap(int *a, int *b);
 void ft_putstr(char *str);
 int ft_strlen(char *str);
 int ft_strcmp(char *s1, char *s2);
+#endif
 
-int main()
+int main(int ac, char **av)
 {
+	(void) ac;
+#if ON
 	ft_putstr("Hello from lib\n");
 	ft_putchar('C');
 	int i = ft_strlen("this is a C09 test");
 	int b = ft_strcmp("hellorc", "hellob");
 	ft_swap(&i, &b);
+#endif
+	char **words_list = ft_split(av[1], av[2]);
+	for(int i = 0; words_list[i]; i++)
+		printf("\"%s\" : %lu\n", words_list[i], strlen(words_list[i]));
+
 	return 0;
 }
