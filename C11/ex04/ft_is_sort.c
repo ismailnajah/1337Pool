03466cd1e0514954bfb9856adea1a42f3ce27c70
Ismail Najah <inajah@e1r8p6.1337.ma>
2024-07-14 13:04:04 +0100

    [Fixed] C11: ex04 check sort in ascending an desending order

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -6,11 +6,25 @@
 /*   By: inajah <inajah@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 09:28:33 by inajah            #+#    #+#             */
-/*   Updated: 2024/07/14 09:40:53 by inajah           ###   ########.fr       */
+/*   Updated: 2024/07/14 13:03:31 by inajah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_is_sort(int *tab, int length, int (*f)(int, int))
+int	ft_is_sort_desc(int *tab, int length, int (*f)(int, int))
+{
+	int	i;
+
+	i = 0;
+	while (i < length - 1)
+	{
+		if ((*f)(tab[i], tab[i + 1]) < 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_is_sort_asc(int *tab, int length, int (*f)(int, int))
 {
 	int	i;
 
@@ -22,4 +36,14 @@ int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 		i++;
 	}
 	return (1);
+}
+
+int	ft_is_sort(int *tab, int length, int (*f)(int, int))
+{
+	int	asc;
+	int	desc;
+
+	asc = ft_is_sort_asc(tab, length, f);
+	desc = ft_is_sort_desc(tab, length, f);
+	return (asc || desc);
 }
