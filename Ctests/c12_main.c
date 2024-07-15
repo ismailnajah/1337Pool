211dee6120c1e81d5bcdd252f0b6f33916ba10a8
Ismail Najah <inajah@e1r2p6.1337.ma>
2024-07-15 21:08:07 +0100

    [Solved] C12: ex16

--- Ctests/c12_main.c
+++ Ctests/c12_main.c
@@ -15,6 +15,7 @@
 #include "../C12/ex13/ft_list_merge.c"
 #include "../C12/ex14/ft_list_sort.c"
 #include "../C12/ex15/ft_list_reverse_fun.c"
+#include "../C12/ex16/ft_sorted_list_insert.c"
 
 #include "../C09/ex02/ft_split.c"
 
@@ -186,7 +187,13 @@ int main()
 	ft_list_foreach(list, &ft_putint);
 
 	printf("\ntesting ft_list_reverse_fun\n");
-	ft_list_reverse_fun(NULL);
+	ft_list_reverse_fun(list);
+	ft_list_foreach(list, &ft_putint);
+
+	printf("\ntesting ft_sorted_list_insert\n");
+	ft_list_reverse_fun(list);
+	nb = 10000000;
+	ft_sorted_list_insert(&list, node(nb), &cmp);
 	ft_list_foreach(list, &ft_putint);
 
 	printf("\n## free the lists ##\n");
